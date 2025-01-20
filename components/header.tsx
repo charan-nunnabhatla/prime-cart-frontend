526ec023a549522b547f09daaf5ed9ac15c5cdb8
Charan <charan.reings125@gmail.com>
2025-01-20 11:42:27 +0530

    header component

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header
+      id="header"
+      className="w-full lg:w-[60%] mx-auto px-3 py-2 h-fit bg-transparent">
+      <span className="text-xl font-bold text-white">Prime Cart</span>
+    </header>
+  );
+}
