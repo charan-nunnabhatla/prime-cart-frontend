4c8d1394c15a3ae7dad358eb4e4b9df5f07f2869
Charan <charan.reings125@gmail.com>
2025-01-26 14:29:03 +0530

    fetches the data from server

--- network/fetch-data.ts
+++ network/fetch-data.ts
@@ -1,4 +1,23 @@
-export default function fetchData(title: string) {
-  console.log(title);
-  
+import { fetchProductData, productDataType } from "@/atoms";
+import { SetStateAction, useSetAtom } from "jotai";
+
+export default async function fetchData(title: string ) {
+  // const setProdutsData = useSetAtom(fetchProductData);
+  //   console.log(title);
+
+  const path = process.env.NEXT_PUBLIC_ngrokURL as string;
+
+  const res = await fetch(path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // title: title,
+    },
+    body: JSON.stringify({ title }),
+  });
+
+  const data: productDataType[] = await res.json();
+  console.log('from fetch method ', data);
+  // setProdutsData(Object.entries(data));
+  return data;
 }
