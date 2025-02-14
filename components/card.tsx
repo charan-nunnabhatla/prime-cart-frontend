cacc0ef3532d77c752bb2ca96d5f5536c122eef2
Charan <charan.reings125@gmail.com>
2025-02-14 10:33:50 +0530

    img size update

--- components/card.tsx
+++ components/card.tsx
@@ -5,7 +5,7 @@ export default function Card(props: productDataType) {
   console.log("Card prorps: ", props);
   return (
     <div key={props.asin} className="rounded h-auto w-fit p-2 bg-orange-300">
-      <Image alt={props.title} src={props.imgURL as string} width={150} height={150} className=" object-cover "/>
+      <Image alt={props.title} src={props.imgURL as string} width={150} height={150} className=" object-cover size-auto"/>
     </div>
   );
 }
