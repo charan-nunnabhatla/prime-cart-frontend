bbc67f36540fbcbe7e97b9d839835f73cb515c1c
Charan <charan.reings125@gmail.com>
2025-02-14 10:37:44 +0530

    new impots

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,23 @@
 "use client";
+import { fetchProductData, productDataType } from "@/atoms";
 
 // import dynamic from "next/dynamic";
 import { CiSearch } from "react-icons/ci";
 import fetchData from "@/network/fetch-data";
-import { ChangeEvent } from "react";
+import { ChangeEvent, FormEvent } from "react";
+import { useSetAtom } from "jotai";
 
 // const Player = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 export default function Canvas() {
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-   await fetchData(e.target.value)
+  const setProdutsData = useSetAtom(fetchProductData);
+
+  const handleChange = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formdata = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formdata.entries());
+    const result = await fetchData(data["search-input"] as string);
+    setProdutsData(result);
   };
 
   return (
@@ -20,15 +28,20 @@ export default function Canvas() {
         Search Across 1.4M+ Products
       </h1>
       <div className="flex flex-col justify-start h-full w-full items-center p-3">
-        <div className="relative group h-10 w-3/4 md:w-1/2 lg:w-2/5 rounded-full">
+        <form
+          onSubmit={(e) => handleChange(e)}
+          id="input-section"
+          className=" mb-4 relative group h-10 w-3/4 md:w-1/2 lg:w-2/5 rounded-full">
           <input
             type="text"
+            name="search-input"
             placeholder="Search"
-            onChange={(e) => handleChange(e)}
             className="border-none focus:ring focus:ring-blue-400 duration-150 outline-none group-hover:ring-blue-400 group-hover:ring w-full h-full px-4 py-2 text-xl rounded-full "
           />
-          <CiSearch className="hover:text-blue-400 duration-150 absolute top-[50%] -translate-y-[50%] right-5 text-2xl font-extrabold hover:cursor-pointer" />
-        </div>
+          <button type="submit">
+            <CiSearch className="hover:text-blue-400 duration-150 absolute top-[50%] -translate-y-[50%] right-5 text-2xl font-extrabold hover:cursor-pointer" />
+          </button>
+        </form>
         {/* <div className=" absolute left-[20%] top-[50%] p-3 size-fit">
           <Player
             animationData={fruitBasketData}
