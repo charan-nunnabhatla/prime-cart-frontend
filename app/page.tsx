a40d13626cad8b4c470fd1f06fd67cef0e784505
Charan <charan.reings125@gmail.com>
2025-02-14 10:33:12 +0530

    height adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
     <main id="root" className="h-full w-full min-h-svh ">
       <div
         id="canva-root"
-        className="relative h-[40%] w-full bg-gradient-to-b from-green-600 to-green-400">
+        className="relative h-fit w-full bg-gradient-to-b from-green-600 to-green-400">
         <Canvas />
       </div>
       <div className=" grid grid-cols-[repeat(auto-fill,minmax(320px,auto))] gap-2 h-fit w-full p-3 bg-blue-400">
-        {productData.map((value: productDataType) => {
+        {productData?.map((value: productDataType) => {
           return <Card key={value.asin} {...value} />;
         })}
       </div>
